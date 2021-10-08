107032f59f1f5fc9993c7ef829d7f730d8e5840d
Alex <alexthomas@berkeley.edu>
2021-10-08 19:22:58 +0000

    Add YCSB + update build

--- build-redis.sh
+++ build-redis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 make distclean
-make CC=riscv64-unknown-linux-musl-gcc MALLOC=libc CFLAGS="-static-pie -static -static-libgcc -static-libstdc++" EXEEXT="-static-pie-static -static-libgcc -static-libstdc++" LDFLAGS="-I/usr/local/include/ -static" -j4
-cp src/redis-server ../build/
-cp redis.conf ../build/
+make CC=riscv64-unknown-linux-musl-gcc MALLOC=libc CFLAGS="-g -static -l-libgcc -static-libstdc++" EXEEXT="-static-pie-static -static-libgcc -static-libstdc++" LDFLAGS="-I/usr/local/include/ -static" -j4
+cp src/redis-server ~/keystone/build/overlay/root/
+cp src/redis-cli ~/keystone/build/overlay/root/
+cp redis.conf ~/keystone/build/overlay/root/
+cp script.redis ~/keystone/build/overlay/root/
